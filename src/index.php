df69921c3a00e3c9eba62e5d7985fa8952643ccb
Md. Shaiful Islam <bappybd@gmail.com>
2018-04-19 14:55:48 +0600

    Adding database connection checking

--- src/index.php
+++ src/index.php
@@ -1,2 +1,16 @@
 <?php
 echo "Docker My custom site working";
+echo "<br /><br />";
+
+// Connect to database
+try {
+    if($pdo = new \PDO(
+        'mysql:host=mydatabase;dbname=my_database',
+        'myuser',
+        'pass'
+    )) {
+        echo "Database connection successfull";
+    }
+} catch (PDOException $e) {
+    echo "Error: " .$e->getMessage();
+}
